5b5f07cb9682ebe294536ae7b944f824cbc47c0d
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2020-08-01 19:04:16 -0700

    Added event emission for download stream, also returns PassThrough stream instead of Promise

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const ytdl = require('ytdl-core');
 const prism = require('prism-media');
+const { PassThrough } = require('stream');
 
 function filter(format) {
 	return format.codecs === 'opus' &&
 		format.container === 'webm' &&
 		format.audioSampleRate == 48000;
 }
+
+function createStream (options) {
+	const stream = new PassThrough({
+		highWaterMark: (options && options.highWaterMark) || 1024 * 512,
+	});
+	stream.destroy = () => { stream._isDestroyed = true; };
+	return stream;
+};
 
 /**
  * Tries to find the highest bitrate audio-only format. Failing that, will use any available audio format.
@@ -23,44 +32,65 @@ function nextBestFormat(formats, isLive) {
 }
 
 function download(url, options = {}) {
-	return new Promise((resolve, reject) => {
-		ytdl.getInfo(url)
-		.then(info => {
-			// Prefer opus
-			const format = info.formats.find(filter);
-			const canDemux = format && info.videoDetails.lengthSeconds != 0;
-			if (canDemux) options = { ...options, filter };
-			else if (info.videoDetails.lengthSeconds != 0) options = { ...options, filter: 'audioonly' };
-			if (canDemux) {
-				const demuxer = new prism.opus.WebmDemuxer();
-				return resolve(ytdl.downloadFromInfo(info, options).pipe(demuxer).on('end', () => demuxer.destroy()));
-			} else {
-				const bestFormat = nextBestFormat(info.formats, info.player_response.videoDetails.isLiveContent);
-				if (!bestFormat) return reject('No suitable format found');
-				const transcoder = new prism.FFmpeg({
-					args: [
-						'-reconnect', '1',
-						'-reconnect_streamed', '1',
-						'-reconnect_delay_max', '5',
-						'-i', bestFormat.url,
-						'-analyzeduration', '0',
-						'-loglevel', '0',
-						'-f', 's16le',
-						'-ar', '48000',
-						'-ac', '2',
-					],
-				});
-				const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
-				const stream = transcoder.pipe(opus);
-				stream.on('close', () => {
-					transcoder.destroy();
-					opus.destroy();
-				});
-				return resolve(stream);
+	const stream = createStream(options);
+	ytdl.getInfo(url)
+	.then(info => {
+		getInfoCallback(stream, info, options);
+	}, stream.emit.bind(stream, 'error'));
+	return stream;
+}
+
+function getInfoCallback(stream, info, options) {
+	// Prefer opus
+	const format = info.formats.find(filter);
+	const canDemux = format && info.videoDetails.lengthSeconds != 0;
+	if (canDemux) options = { ...options, filter };
+	else if (info.videoDetails.lengthSeconds != 0) options = { ...options, filter: 'audioonly' };
+	if (canDemux) {
+		stream.emit('info', info, format);
+		if (stream._isDestroyed) return;
+		const demuxer = new prism.opus.WebmDemuxer();
+		const ytdlDownload = ytdl.downloadFromInfo(info, options)
+		stream.destroy = () => {
+			stream._isDestroyed = true;
+			demuxer.destroy();
+		}
+		ytdlDownload.pipe(demuxer).on('data', data => {
+			if (stream._isDestroyed) return;
+			stream.write(data);
+		}).on('end', () => {
+			demuxer.destroy();
+		});
+	} else {
+		const bestFormat = nextBestFormat(info.formats, info.player_response.videoDetails.isLiveContent);
+		if (!bestFormat) throw new Error('No suitable format found');
+		stream.emit('info', info, bestFormat);
+		if (stream._isDestroyed) return;
+		const transcoder = new prism.FFmpeg({
+			args: [
+				'-reconnect', '1',
+				'-reconnect_streamed', '1',
+				'-reconnect_delay_max', '5',
+				'-i', bestFormat.url,
+				'-analyzeduration', '0',
+				'-loglevel', '0',
+				'-f', 's16le',
+				'-ar', '48000',
+				'-ac', '2',
+			],
+		});
+		const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
+		transcoder.pipe(opus).on('data', data => {
+			if (stream._isDestroyed) {
+				opus.end();
+				return;
 			}
-		})
-		.catch(err => { return reject(err) });
-	});
+			stream.write(data);
+		}).on('close', () => {
+			transcoder.destroy();
+			opus.destroy();
+		});
+	}
 }
 
 module.exports = Object.assign(download, ytdl);
